5bbd0c43517c00869b6f5917f1c53a7bbe1ce154
Arashvir Singh Jamwal <avsjnk@gmail.com>
2023-05-25 00:47:53 +0530

    ok

---
+++ alchemy/JS/TheBasics/DataStorage/2WorkingArray/2HasOne/hasOne.js
@@ -0,0 +1,14 @@
+function hasOne(array) {
+
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === 1){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+module.exports = hasOne;
+
+// Complete the function hasOne which takes in an array of numbers. Return true if any of the numbers in the array are 1. Return false if not.
